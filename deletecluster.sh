7e87028db24139602931726d54ba7b5082ae7788
Abher <119057252+i212498@users.noreply.github.com>
2024-03-03 21:52:28 +0500

    Update deletecluster.sh

--- deletecluster.sh
+++ deletecluster.sh
@@ -8,12 +8,16 @@ fi
 
 no_of_containers=$1
 
-
 container_names=""
+
+sudo docker exec node1 bash -c 'rm /home/storage/machinefile'
+
 for (( i=1; i<=$no_of_containers; i++ )); do
     container_names+="node$i "
+    sudo docker exec "node$i" bash -c 'rm /home/storage/node'"$i"''
 done
 
 
-sudo docker stop $container_names && sudo docker rm $container_names
 
+
+sudo docker stop $container_names && sudo docker rm $container_names
